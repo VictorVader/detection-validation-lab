b66d3e49addd3049372d43f6d2afaf0c983d287b
VictorVader <53708808+VictorVader@users.noreply.github.com>
2026-09-24 12:33:20 +0300

    first detection: hardcoded check for encoded PowerShell

--- read_events.py
+++ read_events.py
@@ -1,6 +1,12 @@
 import json
 
+def is_encoded_powershell(event):
+    return "powershell" in event["process"] and "-enc" in event["command"]
+
 with open("data/events.jsonl") as f:
     for line in f:
         event = json.loads(line)
-        print(event)
+        if is_encoded_powershell(event):
+            print("ALERT:", event)
+        else:
+            print("ok:   ", event)
